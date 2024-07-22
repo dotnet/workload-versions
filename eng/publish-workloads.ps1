5a8625d158686187dad1c44e53bba41ce618509b
Michael Yanni <miyanni@microsoft.com>
2024-07-22 15:18:42 -0700

    Fixed tools path. Clean up commented script.

--- eng/publish-workloads.ps1
+++ eng/publish-workloads.ps1
@@ -1,45 +1,15 @@
-# `tools.ps1` checks $ci to perform some actions. Since the post-build scripts don't necessarily execute in the same agent that run the build.ps1/sh script this variable isn't automatically set.
+# Darc access copied from: eng/common/post-build/publish-using-darc.ps1
 $ci = $true
 $disableConfigureToolsetImport = $true
-. $PSScriptRoot\eng\common\tools.ps1
+. $PSScriptRoot\common\tools.ps1
 
 $darc = Get-Darc
 
 $versionDetailsPath = (Get-Item "$PSScriptRoot\Version.Details.xml").FullName
 $versionDetailsXml = [Xml.XmlDocument](Get-Content $versionDetailsPath)
-
 $versionDetails = $versionDetailsXml.Dependencies.ProductDependencies.Dependency | Select-Object -Property Uri, Sha -Unique
-# $workloadInfo = $versionDetails | Where-Object {
-#   ($_.Uri -Like '*runtime*') -Or
-#   ($_.Uri -Like '*emsdk*') -Or
-#   ($_.Uri -Like '*aspire*') -Or
-#   ($_.Uri -Like '*xamarin-android*') -Or
-#   ($_.Uri -Like '*xamarin-macios*') -Or
-#   ($_.Uri -Like '*maui*')
-# }
-
-# $runtime = $versionDetailsXml.Dependencies.ProductDependencies.Dependency | Where-Object -Property Uri -Like '*runtime*'
-# $emsdk = $versionDetailsXml.Dependencies.ProductDependencies.Dependency | Where-Object -Property Uri -Like '*emsdk*'
-# $aspire = $versionDetailsXml.Dependencies.ProductDependencies.Dependency | Where-Object -Property Uri -Like '*aspire*'
-# $android = $versionDetailsXml.Dependencies.ProductDependencies.Dependency | Where-Object -Property Uri -Like '*xamarin-android*'
-# $macios = $versionDetailsXml.Dependencies.ProductDependencies.Dependency | Where-Object -Property Uri -Like '*xamarin-macios*'
-# $maui = $versionDetailsXml.Dependencies.ProductDependencies.Dependency | Where-Object -Property Uri -Like '*maui*'
-
-# $workloadInfo = $versionDetailsXml.Dependencies.ProductDependencies.Dependency | Where-Object {
-#   ($_.Uri -Like '*runtime*') -Or
-#   ($_.Uri -Like '*emsdk*') -Or
-#   ($_.Uri -Like '*aspire*') -Or
-#   ($_.Uri -Like '*xamarin-android*') -Or
-#   ($_.Uri -Like '*xamarin-macios*') -Or
-#   ($_.Uri -Like '*maui*')
-# }
-
-
 
 $workloadOutputPath = "$PSScriptRoot\..\artifacts\workloads"
-# $darcArgs = @('gather-drop', '--include-released', '--repo', $_.Uri, '--commit', $_.Sha, '--output-dir', $workloadOutputPath)
-# $versionDetails | & $darc $darcArgs
-# $versionDetails | & $darc gather-drop --include-released --repo $_.Uri --commit $_.Sha --output-dir $workloadOutputPath
 $versionDetails | ForEach-Object { & $darc gather-drop --include-released --repo $_.Uri --commit $_.Sha --output-dir $workloadOutputPath }
 
 # if ($LastExitCode -ne 0) {
