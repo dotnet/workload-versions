f0fc63ec6b79686a3a1d600a73afe4eec7a1faa3
Michael Yanni <miyanni@microsoft.com>
2024-10-03 13:22:41 -0700

    Moved all the logic for doing the build to the download-workloads.ps1. This will be renamed in a future commit. Made the official.yml finally able to create the VS drops, but needs tested.

--- eng/download-workloads.ps1
+++ eng/download-workloads.ps1
@@ -1,14 +1,15 @@
 # param ([Parameter(Mandatory=$true)] [SecureString] $gitHubPat, [Parameter(Mandatory=$true)] [SecureString] $azDevPat, [Parameter(Mandatory=$true)] [SecureString] $password)
-param ([Parameter(Mandatory=$true)] [string] $workloadOutputPath, [SecureString] $gitHubPat, [SecureString] $azDOPat)
+param ([Parameter(Mandatory=$true)] [string] $workloadOutputPath, [Parameter(Mandatory=$true)] [string] $msBuildToolsPath, [SecureString] $gitHubPat, [SecureString] $azDOPat)
 
-# Local usage of this script
+# Local Build
+# Local build requires the installation of DARC. See: https://github.com/dotnet/arcade/blob/main/Documentation/Darc.md#setting-up-your-darc-client
 $darc = 'darc'
 $ciArguments = ''
+$ci = $gitHubPat -and $azDOPat
 
-# CI usage of this script
-if ($gitHubPat -and $azDOPat) {
+# CI Build
+if ($ci) {
   # Darc access copied from: eng/common/post-build/publish-using-darc.ps1
-  $ci = $true
   $disableConfigureToolsetImport = $true
   . $PSScriptRoot\common\tools.ps1
 
@@ -20,7 +21,7 @@ if ($gitHubPat -and $azDOPat) {
 #     --password $passwordPlain
 }
 
-# Reads the Version.Details.xml file and downloads the workload drops
+# Reads the Version.Details.xml file and downloads the workload drops.
 $versionDetailsPath = (Get-Item "$PSScriptRoot\Version.Details.xml").FullName
 $versionDetailsXml = [Xml.XmlDocument](Get-Content $versionDetailsPath)
 $versionDetails = $versionDetailsXml.Dependencies.ProductDependencies.Dependency | Select-Object -Property Uri, Sha -Unique
@@ -37,14 +38,57 @@ $versionDetails | ForEach-Object {
     --use-azure-credential-for-blobs
 }
 
-Write-Host 'Downloaded:'
+Write-Host 'Workload drops downloaded:'
 # https://stackoverflow.com/a/9570030/294804
-Get-ChildItem $workloadOutputPath -File -Recurse | Select-Object -Expand FullName
+Get-ChildItem $workloadOutputPath -File -Include 'Workload.VSDrop.*.zip' -Recurse | Select-Object -Expand FullName
 
-# Extracts the workload drops
+# Extracts the workload drop zips.
 $workloads = Get-ChildItem $workloadOutputPath -Include 'Workload.VSDrop.*.zip' -Recurse
-$dropPath = (New-Item "$workloadOutputPath\drops" -Type Container -Force).FullName
-$workloads | ForEach-Object { Expand-Archive -Path $_.FullName -DestinationPath "$dropPath\$([IO.Path]::GetFileNameWithoutExtension($_.Name))" -Force }
+$workloadDropPath = (New-Item "$workloadOutputPath\drops" -Type Container -Force).FullName
+$null = $workloads | ForEach-Object { Expand-Archive -Path $_.FullName -DestinationPath "$workloadDropPath\$([IO.Path]::GetFileNameWithoutExtension($_.Name))" -Force }
 
-Write-Host 'Drop:'
-Get-ChildItem $dropPath -Directory -Recurse | Select-Object -Expand FullName
+# Write-Host 'Drop:'
+# Get-ChildItem $workloadDropPath -Directory -Recurse | Select-Object -Expand FullName
+
+# Extracts the workload drop metadata from the drop name and builds the .vsmanproj project.
+# full: The full drop name, excluding the 'Workload.VSDrop.' prefix.
+# short: The short name of the drop. Only contains the first word after 'Workload.VSDrop.'.
+# type: Either 'pre.components', 'components', or 'packs'.
+$dropInfoRegex = '^Workload\.VSDrop\.(?<full>(?<short>\w*)\..*?(?<type>(pre\.)?components$|packs$))'
+Get-ChildItem -Path $workloadDropPath -Directory | ForEach-Object {
+  $null = $_.Name -match $dropInfoRegex
+  # $Matches.full + ';' + $Matches.short + ';' + $Matches.type.Replace('.', '')
+
+  $assemblyName = "$($Matches.full)"
+  $vsDropName = "Products/dotnet/workloads/$($assemblyName)/$(Get-Date -Format 'yyyyMMdd.hhmmss.fff')"
+  $dropDir = "$($_.FullName)\"
+
+  & "$msBuildToolsPath\MSBuild.exe" Microsoft.NET.Workloads.Vsman.vsmanproj /restore /t:Build `
+    /p:VstsDropNames=$vsDropName `
+    /p:VsixOutputPath=$dropDir `
+    /p:AssemblyName=$assemblyName
+
+  if ($ci) {
+    $shortName = "$($Matches.short)"
+    # Remove the '.' from 'pre.components'
+    $dropType = $Matches.type.Replace('.', '')
+    Write-Host "##vso[task.setvariable variable=$($shortName)_$($dropType)_name]$vsDropName"
+    Write-Host "##vso[task.setvariable variable=$($shortName)_$($dropType)_dir]$dropDir"
+  }
+
+  Write-Host 'After upload, your workload drop will be available at:'
+  Write-Host "https://devdiv.visualstudio.com/_apps/hub/ms-vscs-artifact.build-tasks.drop-hub-group-explorer-hub?name=$vsDropName"
+}
+
+# Clean up intermediate build files in the workload drop folders.
+$null = Get-ChildItem -Path $workloadDropPath -Include *.json, *.vsmand, files.txt -Recurse | Remove-Item
+
+
+# set DropName=Products/dotnet/workloads/%(WorkloadInfo.Identity)/$([System.DateTime]::Now.ToString("yyyyMMdd.hhmmss.fff")) &amp;&amp; ^
+# set DropDir=$(WorkloadDropDirectory)Workload.VSDrop.%(WorkloadInfo.Identity)\ &amp;&amp; ^
+# "$(MSBuildToolsPath)\MSBuild.exe" Microsoft.NET.Workloads.Vsman.vsmanproj /restore /t:Build ^
+#   /p:VstsDropNames=%DropName% ^
+#   /p:VsixOutputPath=%DropDir% ^
+#   /p:AssemblyName=%(WorkloadInfo.Identity) &amp;&amp; ^
+# echo ##vso[task.setvariable variable=%(WorkloadInfo.Short)_%(WorkloadInfo.Type)_name]%DropName% &amp;&amp; ^
+# echo ##vso[task.setvariable variable=%(WorkloadInfo.Short)_%(WorkloadInfo.Type)_dir]%DropDir%
