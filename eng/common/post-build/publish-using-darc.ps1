e04db40c5345401436370d511b12bedcffddecbc
Marc Paine <marcpop@microsoft.com>
2025-05-12 11:32:10 -0700

    Revert the arcade 10 update

--- eng/common/post-build/publish-using-darc.ps1
+++ eng/common/post-build/publish-using-darc.ps1
@@ -5,9 +5,7 @@ param(
   [Parameter(Mandatory=$false)][string] $MaestroApiEndPoint = 'https://maestro.dot.net',
   [Parameter(Mandatory=$true)][string] $WaitPublishingFinish,
   [Parameter(Mandatory=$false)][string] $ArtifactsPublishingAdditionalParameters,
-  [Parameter(Mandatory=$false)][string] $SymbolPublishingAdditionalParameters,
-  [Parameter(Mandatory=$false)][string] $RequireDefaultChannels,
-  [Parameter(Mandatory=$false)][string] $SkipAssetsPublishing
+  [Parameter(Mandatory=$false)][string] $SymbolPublishingAdditionalParameters
 )
 
 try {
@@ -34,14 +32,6 @@ try {
 
   if ("false" -eq $WaitPublishingFinish) {
     $optionalParams.Add("--no-wait") | Out-Null
-  }
-  
-  if ("true" -eq $RequireDefaultChannels) {
-    $optionalParams.Add("--default-channels-required") | Out-Null
-  }
-
-  if ("true" -eq $SkipAssetsPublishing) {
-    $optionalParams.Add("--skip-assets-publishing") | Out-Null
   }
 
   & $darc add-build-to-channel `
