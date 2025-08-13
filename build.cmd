adb4b3628a70b86b22133794b9d707c97acb539e
Michael Yanni <miyanni@microsoft.com>
2025-08-12 18:48:36 -0700

    Made robocopy not emit anything and made it exclude some more files.

--- build.cmd
+++ build.cmd
@@ -1,7 +1,7 @@
 @echo off
 for /f %%i in ('git config --get remote.origin.url') do set REPO_URL=%%i
 git clone -b eng %REPO_URL% eng-branch
-robocopy "eng-branch" "." /E /XD ".git" ".config" /XF ".gitignore"
+robocopy "eng-branch" "." /E /XD ".git" ".config" /XF ".gitignore" "build.cmd" "public.yml" /NJH /NJS /NP /NFL /NDL
 rmdir /s /q "eng-branch"
 powershell -NoLogo -NoProfile -ExecutionPolicy ByPass -Command "& """%~dp0eng\common\build.ps1""" -restore -build -msbuildEngine vs %*"
 exit /b %ErrorLevel%
