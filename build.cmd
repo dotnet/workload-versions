cae9c2aac9f2ea77f9cc5ec3dbac83e41d8daec9
Michael Yanni <miyanni@microsoft.com>
2025-08-12 18:13:59 -0700

    Determining how to do local builds.

--- build.cmd
+++ build.cmd
@@ -1,3 +1,7 @@
 @echo off
+for /f %%i in ('git config --get remote.origin.url') do set REPO_URL=%%i
+git clone -b eng %REPO_URL% eng-branch
+robocopy "eng-branch" "." /E /XD ".git" ".config" /XF ".gitignore"
+rmdir /s /q "eng-branch"
 powershell -NoLogo -NoProfile -ExecutionPolicy ByPass -Command "& """%~dp0eng\common\build.ps1""" -restore -build -msbuildEngine vs %*"
 exit /b %ErrorLevel%
