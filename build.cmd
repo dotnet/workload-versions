c4f9a8bd39fc93ddee2bffc950ffdae83511360b
Michael Yanni <miyanni@microsoft.com>
2025-08-14 16:14:01 -0700

    Fixed doing the local build by storing the arguments and skipping the first argument. Fixed incorrect Versions.props path.

--- build.cmd
+++ build.cmd
@@ -5,11 +5,12 @@ if "%1"=="" (
     exit /b 1
 )
 set SOURCE_BRANCH=%1
-rem Remove sourceBranch from argument list so it's not passed to the build.ps1 script.
-shift
+rem Keep the arguments, excluding the first (SOURCE_BRANCH)
+set "REMAINING_ARGS="
+for /f "tokens=1* delims= " %%a in ("%*") do set "REMAINING_ARGS=%%b"
 for /f %%i in ('git config --get remote.origin.url') do set REPO_URL=%%i
 git clone -b %SOURCE_BRANCH% %REPO_URL% source-branch
 robocopy "source-branch" "." /E /XO /XD ".git" ".config" /XF ".gitignore" "build.cmd" "public.yml" /NJH /NJS /NP /NFL /NDL
 rmdir /s /q "source-branch"
-powershell -NoLogo -NoProfile -ExecutionPolicy ByPass -Command "& """%~dp0eng\common\build.ps1""" -restore -build -msbuildEngine vs %*"
+powershell -NoLogo -NoProfile -ExecutionPolicy ByPass -Command "& """%~dp0eng\common\build.ps1""" -restore -build -msbuildEngine vs %REMAINING_ARGS%"
 exit /b %ErrorLevel%
