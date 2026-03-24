f391010ec290d9883afa8603ca78877cc53f6b5a
Marc Paine <marcpop@microsoft.com>
2026-03-24 15:50:50 -0700

    Change asset filter to match Microsoft packages

--- eng/download-workloads-nugets.ps1
+++ eng/download-workloads-nugets.ps1
@@ -55,7 +55,7 @@ if ($workloadListJson) {
 }
 
 # Asset filter for .nupkg files.
-$assetFilter = '^[^/]+'
+$assetFilter = '^Microsoft\.'
 Write-Host "assetFilter: $assetFilter"
 
 # Runs DARC against each workload build to download the NuGet packages (if applicable based on the filter).
